38163ffc58e2d37f52d1cc91120d4b0eb969fdd0
Mohamed Aly <mohamedalyahmed@aol.com>
2022-11-02 16:50:31 +0200

    edit data on db.json file & Card (Shared Component)

--- src/Shared/Card/Card.jsx
+++ src/Shared/Card/Card.jsx
@@ -1,38 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Card.css";
 
-export default class Card extends Component {
-  render() {
-    return (
-      <div>
-        <div className="card " style={{ width: "14rem" }}>
-          <img
-            src={require("../../assets/html5.jpg")}
-            className="card-img-top "
-            alt="course image"
-          />
-          <div className="card-body position-relative">
-            <div className=" text-light duration animate__fadeInRightBig">
-              12h 20min / 11 lessons
-            </div>
-            <h5 className="card-title mb-4">Course title</h5>
-            <div className="d-flex justify-content-between align-items-center">
-              <p
-                className="card-text align-self-center mb-0 text-secondary"
-                style={{ fontSize: 14 }}
-              >
-                instructor Name
-              </p>
-              <button className="btn btn-light">
-                <i
-                  className="fa-light fa-bookmark text-dark"
-                  title="Save for later"
-                ></i>
-              </button>
-            </div>
+export default function Card() {
+  let [courseImg, setCourseImg] = useState("/assets/html5.jpg");
+  let [title, setTitle] = useState("HTML5");
+  let [hour, setHour] = useState(12);
+  let [minutes, setMinutes] = useState(12);
+  let [lessons, setLessons] = useState(17);
+  let [instructorName, setInstructorName] = useState("Instructor Name");
+
+  return (
+    <div>
+      <div className="card shadow-sm" style={{ width: "14rem" }}>
+        <img src={courseImg} className="card-img-top " alt="course image" />
+        <div className="card-body position-relative">
+          <div className=" text-light duration animate__fadeInRightBig">
+            {hour}h {minutes}min / {lessons} lessons
+          </div>
+          <h5 className="card-title mb-4">{title}</h5>
+          <div className="d-flex justify-content-between align-items-center">
+            <p
+              className="card-text align-self-center mb-0 text-secondary"
+              style={{ fontSize: 14 }}
+            >
+              {instructorName}
+            </p>
+            <button className="btn btn-light">
+              <i
+                className="fa-light fa-bookmark text-dark"
+                title="Save for later"
+              ></i>
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
