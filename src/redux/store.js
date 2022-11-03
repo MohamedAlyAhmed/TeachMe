89409b802dfe5a60a8bd79726f2f3c650611a0b7
ahmedmagdymohamedy <ahmedmagdymohamedy@gmail.com>
2022-11-03 17:28:26 +0200

    add server reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './reducers/counter'
+import server from './reducers/server'
 
 export default configureStore({
     reducer: {
         // add new any new reducers here .
         counter: counterReducer,
+        server: server,
     },
 })
