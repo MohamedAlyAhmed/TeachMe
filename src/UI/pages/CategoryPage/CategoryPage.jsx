8a90755233752fe3a4f8dfa3c9e4c4d9e185f64c
Mohamed Aly <mohamedalyahmed@aol.com>
2022-11-07 05:44:37 +0200

    Add Filter(level & language) For Category Page

--- src/UI/pages/CategoryPage/CategoryPage.jsx
+++ src/UI/pages/CategoryPage/CategoryPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import MyButton from "../../components/Button/Button";
 import { DataContext } from "../../../DataContext";
 import CourseCard from "../../components/CourseCard/CourseCard";
@@ -7,8 +7,32 @@ import "./CategoryPage.css";
 export default function CategoryPage() {
   let { courses } = useContext(DataContext);
 
+  const [courseFilter, setCourseFilter] = useState(courses);
+
+  // Filter By ( Level & Language )
+  const optionSelected = () => {
+    let LevelOption = document.querySelector("#levelFilter");
+    let level = LevelOption.value;
+
+    let LanguageOption = document.querySelector("#LanguageFilter");
+    let language = LanguageOption.value;
+
+    if ((level == 1 || level == 2) && (language == 1 || language == 2)) {
+      setCourseFilter(
+        courses.filter((e) => e.level == level && e.courseLanguage == language)
+      );
+    } else if (level == 1 || level == 2) {
+      setCourseFilter(courses.filter((e) => e.level == level));
+    } else if (language == 1 || language == 2) {
+      setCourseFilter(courses.filter((e) => e.courseLanguage == language));
+    } else {
+      setCourseFilter(courses);
+    }
+  };
+
   return (
     <div>
+      {/* Gradient Background  */}
       <div className="gradient-div">
         <p className="category-name mb-4">Soft Skills</p>
         <div className="bg-new-color text-secondary shadow mb-4 rounded-3 d-flex justify-content-between align-items-center p-4">
@@ -17,71 +41,39 @@ export default function CategoryPage() {
           <MyButton>Subscribe</MyButton>
         </div>
       </div>
-
+      {/* Option Filter  */}
       <div className=" w-100 container my-3 ">
-        <div className="d-flex align-items-center ms-3 small-screen ">
+        <div className="d-flex align-items-center ms-3 small-screen option-sm">
           {/* Filter By  */}
-          <div className="fw-bold me-3">Filter:</div>
-          {/* Level Filter  */}
-          <div class="dropdown me-3">
-            <button
-              class="btn btn-light dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Level
-            </button>
-            <ul class="dropdown-menu bg-light">
-              <li>
-                <a class="dropdown-item" href="#">
-                  General
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Specialized
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Expert
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          {/* Language Filter  */}
-          <div class="dropdown me-5">
-            <button
-              class="btn btn-light dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Language
-            </button>
-            <ul class="dropdown-menu bg-light">
-              <li>
-                <a class="dropdown-item" href="#">
-                  Arabic
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  English
-                </a>
-              </li>
-            </ul>
-          </div>
+          <div className="fw-bold me-3">Filter by :</div>
+          {/* Level options  */}
+          <select
+            id="levelFilter"
+            className="form-select w-25 h-25 me-3 option-small"
+            onChange={() => optionSelected()}
+          >
+            <option selected="selected">All Levels</option>
+            <option value={1}>Level 1</option>
+            <option value={2}>Level 2</option>
+          </select>
+          {/* Language options  */}
+          <select
+            id="LanguageFilter"
+            className="form-select w-25 h-25 me-3 option-small"
+            onChange={() => optionSelected()}
+          >
+            <option selected="selected">All languages</option>
+            <option value={1}>Language 1</option>
+            <option value={2}>Language 2</option>
+          </select>
         </div>
       </div>
-
+      {/* Courses Result  */}
       <div className="container w-100">
         <div className="row ">
-          {courses.map((course) => (
+          {courseFilter.map((e) => (
             <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 small-screen">
-              <CourseCard course={course} />
+              <CourseCard course={e} />
             </div>
           ))}
         </div>
