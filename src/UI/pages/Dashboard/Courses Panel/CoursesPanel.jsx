d2304fbe51c9e53d5c1d1de65c10ab04dc516986
Ahmed Mohamed <ahmedabozied699@gmail.co>
2022-11-15 00:19:24 +0200

    add dashboard courses panel

--- src/UI/pages/Dashboard/Courses Panel/CoursesPanel.jsx
+++ src/UI/pages/Dashboard/Courses Panel/CoursesPanel.jsx
@@ -1,8 +1,327 @@
-import React from 'react'
-import './CoursesPanel.css'
+import "./CoursesPanel.css";
+import React, { useState } from "react";
+import axios from "axios";
+import { BASE_URL, DataContext } from "../../../../DataContext";
+import { useContext } from "react";
+import CourseCard from "../../../components/CourseCard/CourseCard";
+// import CoursesUpdate from './CoursesUpdate/CoursesUpdate';
+import { toast } from "react-toastify";
+import CoursesUpdate from "./CoursesUpdate/CoursesUpdate";
 
 export default function CoursesPanel() {
+  const { courses } = useContext(DataContext);
+  // For Add Course
+  const [name, setName] = useState("Course Name");
+  const [permanentLink, setPermanentLink] = useState(
+    "Cinematic-Lighting-Masterclass"
+  );
+  const [category, setcCategory] = useState("Category Name");
+  const [level, setLevel] = useState(1);
+  const [courseLanguage, setCourseLanguage] = useState("Arabic");
+  const [courseIds, setCourseIds] = useState(234);
+  const [description, setDescription] = useState("Description of course");
+  const [mentors, setmentors] = useState("New-Course");
+  const [duration, setDeuration] = useState("4.30");
+  const [numberOfLessons, setNumberOfLessons] = useState("4.30");
+  const [image, setImage] = useState(
+    "https://previews.123rf.com/images/melpomen/melpomen1509/melpomen150900104/45650274-hand-pointing-to-online-course-concept-on-light-brown-wall-background.jpg"
+  );
+
+  const coursePreview = {
+    name: name,
+    category: category,
+    image: image,
+    duration: duration,
+    numberOfLessons: numberOfLessons,
+    mentors: mentors,
+    description: description,
+    courseIds: courseIds,
+    courseLanguage: courseLanguage,
+    level: level,
+    permanentLink: permanentLink,
+  };
+
+  const AddCourse = () => {
+    axios
+      .post(`${BASE_URL}/courses`, {
+        name: name,
+        image: image,
+        mentors: mentors,
+        duration: duration,
+        numberOfLessons: numberOfLessons,
+        category: category,
+        description: description,
+        courseIds: [courseIds],
+        courseLanguage: courseLanguage,
+        level: level,
+        permanentLink: permanentLink,
+      })
+      .then((response) => {
+        console.log(response);
+        toast.success("Courses Added Successefully");
+
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Courses Added Failed !");
+      });
+
+  };
+
+  const deleteCourse = (id) => {
+    if (window.confirm("Are You Sure To Delete This Course ?")) {
+      axios
+        .delete(`${BASE_URL}/courses/${id}`)
+        .then((response) => {
+          console.log(response);
+          toast.success("Courses Deleted Successefully");
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error("Courses Deleted Failed !");
+        });
+
+      refreshPage();
+    } else {
+      console.log("Declined");
+    }
+  };
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor((d % 3600) / 60);
+    var s = Math.floor((d % 3600) % 60);
+
+    var hDisplay = h > 0 ? h + (h == 1 ? "h " : "h ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? "m " : "m ") : "";
+    return hDisplay + mDisplay;
+  }
   return (
-    <div>CoursesPanel</div>
-  )
+    <>
+      <div>
+        <div className="bg-dark text-center display-6 text-light rounded">
+          Courses Panel
+        </div>
+
+        <div className="row mb-5 border shadow p-3">
+          {/* Category Card Preview  */}
+          <div className="col-xl-3 col-lg-5 col-md-6 col-sm-12  d-flex justify-content-center flex-column align-items-center">
+            <h2 className="text-light bg-dark p-2 rounded mt-2">Add Course</h2>
+
+            <CourseCard course={coursePreview} />
+
+            <div className="text-primary text-center">
+              PermanentLink :<br></br> {permanentLink}
+            </div>
+          </div>
+          {/*Add Category Form*/}
+          <form
+            className="col-xl-9 col-lg-7 col-md-6 col-sm-12  mt-5"
+            onSubmit={() => AddCourse()}
+          >
+            {/*(1) name ***/}
+            <div className="mb-3">
+              <label for="category_name" className="form-label">
+                Course Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="category_name"
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
+              <div className="form-text">Please Add Course Name</div>
+            </div>
+            {/*(2) Description */}
+            <div className="mb-3">
+              <label for="category_name" className="form-label">
+                Description
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="category_name"
+                required
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <div className="form-text">Please Add description</div>
+            </div>
+            {/*(3) image*** */}
+            <div className="mb-3">
+              <label for="imageUrl" className="form-label">
+                Image URL
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="imageUrl"
+                required
+                onChange={(e) => setImage(e.target.value)}
+              />
+              <div className="form-text">
+                Like : http://www.google.com/logo.png
+              </div>
+            </div>
+            {/* (4)Mentors */}
+            <div className="w-100 d-flex">
+              <div className="mb-3 w-50">
+                <label for="category_name" className="form-label">
+                  Mentor
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="category_name"
+                  required
+                  onChange={(e) => setmentors(e.target.value)}
+                />
+                <div className="form-text">Please Add Mentor</div>
+              </div>
+              {/* (5) Permanent Link */}
+              <div className="mb-3 w-50 ms-3">
+                <label for="permanentLink" className="form-label">
+                  Permanent Link
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="permanentLink"
+                  required
+                  onChange={(e) => setPermanentLink(e.target.value)}
+                />
+                <div className="form-text">Like : Ayman-Ahmed</div>
+              </div>
+            </div>
+            {/*(6) courseIds */}
+            <div className="mb-3">
+              <label for="permanentLink" className="form-label">
+                courseIds
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="permanentLink"
+                required
+                onChange={(e) => setCourseIds(parseInt(e.target.value))}
+              />
+              <div className="form-text">Like : 903</div>
+            </div>
+            {/*(7) Level */}
+            <div className="mb-3">
+              <label for="permanentLink" className="form-label">
+              Level
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="permanentLink"
+                required
+                onChange={(e) => setLevel(parseInt(e.target.value))}
+              />
+              <div className="form-text">Like : 1</div>
+            </div>
+            {/*(8) Number of lessons */}
+            <div className="mb-3">
+              <label for="permanentLink" className="form-label">
+                Number of Lessons
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="permanentLink"
+                required
+                onChange={(e) => setNumberOfLessons(parseInt(e.target.value))}
+              />
+              <div className="form-text">Like : 1</div>
+            </div>
+            {/*(9) Deuration */}
+            <div className="mb-3">
+              <label for="permanentLink" className="form-label">
+                Deuration
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="permanentLink"
+                required
+                onChange={(e) => setDeuration(parseInt(e.target.value))}
+              />
+              <div className="form-text">Like : 1</div>
+            </div>
+            {/*(10) Course Language */}
+            <div className="mb-3">
+              <label for="category_name" className="form-label">
+                Description
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="category_name"
+                required
+                onChange={(e) => setCourseLanguage(e.target.value)}
+              />
+              <div className="form-text">Please Add Course Language</div>
+            </div>
+            {/*(11) Categoty */}
+
+            <div className="mb-3">
+              <label for="category" className="form-label">
+              Categoty
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="category"
+                required
+                onChange={(e) => setcCategory(e.target.value)}
+              />
+              <div className="form-text">Like : Soft-Skills</div>
+            </div>
+            <button type="submit" className="btn btn-success">
+              Add Course
+            </button>
+          </form>
+        </div>
+
+        <div className="bg-dark text-center display-6 text-light p-2 rounded">
+          Update & Delete Instructor
+        </div>
+
+        <div className="row">
+          {courses.map((e, index) => (
+            <div
+              key={index}
+              className=" col-xl-3 col-lg-4 col-md-6 col-sm-12 col-12 d-flex align-items-center justify-content-center rounded-2 flex-column my-3"
+            >
+              <div className="bg-light p-2 rounded d-flex flex-column justify-content-center align-items-center">
+                <CourseCard course={e} />
+
+                {/* Buttons Delete & Update  */}
+
+                <div className="m-3 d-flex ">
+                  <CoursesUpdate course={e} />
+
+                  <button
+                    type="submit"
+                    className="btn btn-danger mx-2 "
+                    onClick={() => deleteCourse(e.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      
+    </>
+   
+  );
 }
---
+++ src/UI/pages/Dashboard/Courses Panel/CoursesUpdate/CoursesUpdate.jsx
@@ -0,0 +1,241 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useState } from "react";
+import { BASE_URL } from "../../../../../DataContext";
+import CourseCard from "../../../../components/CourseCard/CourseCard";
+import axios from "axios";
+import "./CoursesUpdate.css";
+import { ToastContainer ,toast} from "react-toastify";
+export default function CoursesUpdate({ course }) {
+    // For Update course
+    const [updtedName, setUpdtedName] = useState("Course Name");
+  const [updtedPermanentLink, setUpdtedPermanentLink] = useState(
+    "Cinematic-Lighting-Masterclass"
+  );
+  const [updtedCategory, setcUpdtedCategory] = useState("Category Name");
+  const [updtedLevel, setUpdtedLevel] = useState(1);
+  const [updtedCourseLanguage, setUpdtedCourseLanguage] = useState("Arabic");
+  const [updtedCourseIds, setUpdtedCourseIds] = useState(234);
+  const [updtedDescription, setUpdtedDescription] = useState("Description of course");
+  const [updtedMentors, setUpdtedMentors] = useState("New-Course");
+  const [updtedDuration, setUpdtedDuration] = useState("4.30");
+  const [updtedNumberOfLessons, setUpdtedNumberOfLessons] = useState("4.30");
+  const [updtedImage, setUpdtedImage] = useState(
+    "https://previews.123rf.com/images/melpomen/melpomen1509/melpomen150900104/45650274-hand-pointing-to-online-course-concept-on-light-brown-wall-background.jpg"
+  );
+    // For Preview course
+ const updatedPreview = {
+  name: updtedName,
+  category: updtedCategory,
+  image: updtedImage,
+  duration: updtedDuration,
+  numberOfLessons: updtedNumberOfLessons,
+  mentors: updtedMentors,
+  description: updtedDescription,
+  courseIds: [updtedCourseIds],
+  courseLanguage: updtedCourseLanguage,
+  level: updtedLevel,
+  permanentLink: updtedPermanentLink,
+};
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const updateCourse = (id) => {
+    axios
+      .put(`${BASE_URL}/courses/${id}`, {
+        name: updtedName,
+  category: updtedCategory,
+  image: updtedImage,
+  duration: updtedDuration,
+  numberOfLessons: updtedNumberOfLessons,
+  mentors: updtedMentors,
+  description: updtedDescription,
+  courseIds: [updtedCourseIds],
+  courseLanguage: updtedCourseLanguage,
+  level: updtedLevel,
+  permanentLink: updtedPermanentLink,
+      })
+      .then((response) => {
+        console.log(response);
+        toast.success("Courses Updated Successefully")
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Failed updated Course!")
+      });
+    handleClose();
+    refreshPage();
+  };
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div>
+    <button className="btn btn-primary" onClick={handleClickOpen}>
+        Update
+      </button>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle className="text-center">{course.name}</DialogTitle>
+      <DialogContent>
+        <DialogContentText className="d-flex justify-content-center align-items-center flex-column">
+          <CourseCard course={updatedPreview} />
+          Mentor : {updtedMentors}
+        </DialogContentText>
+
+       {/* course_Name */}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="Course_Name"
+          label="Course_Name"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.name}
+          onChange={(e) => setUpdtedName(e.target.value)}
+        />
+        {/* permanent_Link */}
+         <TextField
+          autoFocus
+          margin="dense"
+          id="permanent_Link"
+          label="permanent_Link"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.permanentLink}
+          onChange={(e) => setUpdtedPermanentLink(e.target.value)}
+        />
+         {/* category */}
+         <TextField
+          autoFocus
+          margin="dense"
+          id="category"
+          label="Category"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.category}
+          onChange={(e) => setcUpdtedCategory(e.target.value)}
+        />
+         {/* level */}
+         <TextField
+          autoFocus
+          margin="dense"
+          id="level"
+          label="level"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.level}
+          onChange={(e) => setUpdtedLevel(e.target.value)}
+        />
+         {/* updtedCourseLanguage */}
+         <TextField
+          autoFocus
+          margin="dense"
+          id="Course_Language"
+          label="Course_Language"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.courseLanguage}
+          onChange={(e) => setUpdtedCourseLanguage(e.target.value)}
+        />
+        {/* updtedCourseIds */}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="Course_Ids"
+          label="Course_Ids"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.courseIds}
+          onChange={(e) => setUpdtedCourseIds(e.target.value)}
+        />
+        {/* updtedDescription */}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="Description"
+          label="Description"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.description}
+          onChange={(e) => setUpdtedDescription(e.target.value)}
+        />
+        {/* updtedMentors */}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="Mentor_Name"
+          label="Mentor_Name"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.mentors.name}
+          onChange={(e) => setUpdtedMentors(e.target.value)}
+        />
+        {/* updtedDuration */}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="Duration"
+          label="Duration"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.duration}
+          onChange={(e) => setUpdtedDuration(e.target.value)}
+        />
+        {/* updtedNumberOfLessons */}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="NumberOfLessons"
+          label="Number_Of_Lessons"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.numberOfLessons}
+          onChange={(e) => setUpdtedNumberOfLessons(e.target.value)}
+        />
+        {/* updtedImage */}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="ImgURL"
+          label="Img_URL"
+          type="text"
+          fullWidth
+          variant="standard"
+          defaultValue={course.image}
+          onChange={(e) => setUpdtedImage(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={() => updateCourse(course.id)}>
+          Save Changes
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </div>
+ 
+  )
+}
