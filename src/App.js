03dfa4836113c20171e1b9a937179dd6e1992eab
Mohamed Aly <mohamedalyahmed@aol.com>
2023-01-05 12:22:29 +0200

    Edit Routes

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ import PersonalInformation from "./UI/pages/UserProfile/Personal/personalInf";
 import SavesList from "./UI/pages/UserProfile/SavedList/SavesList";
 import MyCourses from "./UI/pages/UserProfile/MyCourses/Mycourses";
 
+
 //Landing Page LazyLoading
 const LazyLandingPage = React.lazy(() =>
   import("./UI/pages/LandingPage/langingPage")
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import { HashRouter } from 'react-router-dom';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
